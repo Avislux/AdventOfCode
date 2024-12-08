b81c1d35bab78c0523b89314129dd0bd2d6db12f
Avislux <gameguy94@hotmail.com>
2024-12-08 07:13:16 -0800

    Day 2 and 4 done.

--- 2024/day2.js
+++ 2024/day2.js
@@ -22,6 +22,77 @@ function isLineSafe(line) {
     }
     return true;
 }
+//So for one of the examples 1 3 2 4 5, you would produce the difference array -2 1 -2 -1
+// The second value is obviously bad because all the others are negatives/ascending
+// So to skip it, you just do the bad value + one of the adjacent values
+// Generically, it's the bad value + the value to its right, but one of the edge cases where the first value gets removed instead involves adding to the left
+var count = 0;
+function isLineSafe2(line){
+    let length = line.length;
+    let diffArray = [];
+    let initialLineIsSafe = true;
+    for (let i = 0; i < length - 1; i++) {
+        let diff = line[i] - line[i + 1];
+        trend = diff >= 0; //bool positive = true;
+        if (i == 0) {
+            initialTrend = trend; //bool positive = true;
+        }
+        diffArray.push(diff);
+        if (diff == 0) {
+            initialLineIsSafe = false;
+        }
+        if (Math.abs(diff) > 3) {
+            initialLineIsSafe = false;
+
+        }
+        if (initialTrend != trend) {
+            initialLineIsSafe = false;
+
+        }
+    }
+    if (initialLineIsSafe){
+        return true;
+    }
+    let isSafe = false;
+    console.log(line + " double checking")
+    for (let i = 0; i < length; i++) {
+        let testArray = [];
+        for (let x = 0; x < length; x++) {
+            if (x !== i){
+                testArray.push(line[x]);
+            }
+        }
+        // console.log(testArray);
+        if ( isLineSafe(testArray)){
+            return true;
+        }
+    }
+    return false;
+}
+/*  let diffArrayLength = diffArray.length;
+    let errorCount = 0;
+    for (let i = 0; i < diffArrayLength - 1; i++) {
+        
+        let value = diffArray[i];
+        if (value > 3 || value === 0){
+            errorCount++
+            if (errorCount > 1){
+                return false
+            }
+        }
+        if (value > 3){
+            let adjacentDiff = diffArray[i] + diffArray[i+1];
+            if (Math.abs(adjacentDiff )> 3) {
+                errorCount++
+                console.log("error on " +  diffArray[i] + " " + diffArray[i+1])
+                if (errorCount >1){
+                    return false;
+                }
+               
+            }
+        }
+
+} */
 try {
     const data = fs.readFileSync('inputs/day2.txt', 'utf8');
 
@@ -44,8 +115,9 @@ try {
         var trend = null;
         var initialTrend = null;
         let isSafe = true;
-        isSafe = isLineSafe(line)
-       
+        // isSafe = isLineSafe(line)
+
+        isSafe = isLineSafe2(line)
         if (isSafe){
             safeLines++;
         } else {
@@ -73,5 +145,6 @@ try {
     console.error(err);
 }
 // Solved
-// Part 1: 3714264
-// Part 2: 18805872
+// Part 1: 334
+// Part 2: 793 too high.
+// Part 2: 400 correct
--- 2024/day4.js
+++ 2024/day4.js
@@ -1,6 +1,18 @@
 const fs = require('node:fs');
-const {row} = require("mathjs");
+function arraysEqual(arr1, arr2) {
+    // Check if the lengths are different
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
 
+    // Check each element for equality
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 try {
     const data = fs.readFileSync('inputs/day4.txt', 'utf8');
     let rows = data.split("\r\n")
@@ -17,38 +29,94 @@ try {
     let xmasMatches = 0;
     let samxMatches = 0;
     let horMatches = 0; //435
+    let crossMas = 0;
+    
     for (let y = 0; y < rows.length; y++) {
         //Horizontal checks
         let row = rows[y];
         const array = [...row.matchAll(re)];
         const array2 = [...row.matchAll(re2)];
-        
         let numberMatchesInRow = array.length + array2.length;
         xmasMatches += array.length;
         samxMatches += array2.length;
-        
+
         horMatches += numberMatchesInRow;
         totalMatches += numberMatchesInRow;
         // console.log(totalMatches);
-       
+
         for (let x = 0; x < row.length; x++) {
             if (row[x] === 'X') {
                 //vertical case
-                if (y < rows.length - 3 && rows[y + 1][x] === "M" && rows[y + 2][x] === "A" &&  rows[y + 3][x] === "S") {
+                //
+                if (y < rows.length - 3 && rows[y + 1][x] === "M" && rows[y + 2][x] === "A" && rows[y + 3][x] === "S") {
                     totalMatches++;
                     xmasVertMatches++; //229
                 }
                 //diag down right
-                if (y < rows.length - 3 && x <= row.length - 3 && rows[y + 1][x + 1] === "M" && rows[y + 2][x + 2] === "A" &&  rows[y + 3][x + 3] === "S") {
+                //
+                if (y < rows.length - 3 && x <= row.length - 3 && rows[y + 1][x + 1] === "M" && rows[y + 2][x + 2] === "A" && rows[y + 3][x + 3] === "S") {
                     totalMatches++;
                     xmasDownRightMatches++; //371
                 }
+
                 //down left
-                if (y < rows.length - 3 && x > 2 && rows[y + 1][x - 1] === "M" && rows[y + 2][x - 2] === "A" &&  rows[y + 3][x - 3] === "S") {
+                //
+                if (y < rows.length - 3 && x > 2 && rows[y + 1][x - 1] === "M" && rows[y + 2][x - 2] === "A" && rows[y + 3][x - 3] === "S") {
                     totalMatches++;
                     xmasDownLeftMatches++; //371
                 }
-            } else if (row[y] === 'S') {
+
+                //up left
+                //
+                if (y > 2 && x > 2 && rows[y - 1][x - 1] === "M" && rows[y - 2][x - 2] === "A" && rows[y - 3][x - 3] === "S") {
+                    totalMatches++;
+                    samxDownRightMatches++; //431
+                }
+                //up right
+                //
+                if (y > 2 && x <= row.length - 3 && rows[y - 1][x + 1] === "M" && rows[y - 2][x + 2] === "A" && rows[y - 3][x + 3] === "S") {
+                    totalMatches++;
+                    samxDownLeftMatches++; //409
+                }
+                //
+                if (y > 2 && rows[y - 1][x] === "M" && rows[y - 2][x] === "A" && rows[y - 3][x] === "S") {
+                    totalMatches++;
+                    samxVertMatches++; //215
+                }
+
+            } else if ( (row[x] === 'A') ) {
+                try {
+                    let check1 = [ rows[y - 1][x - 1], rows[y + 1][x + 1] ];
+                    let check2 = [ rows[y + 1][x - 1], rows[y - 1][x + 1] ];
+                    check1.sort( );
+                    check2.sort( );
+                    if (arraysEqual(check1, ["M","S"]) && arraysEqual(check2, ["M","S"] ) ) {
+                        crossMas++;
+                    }
+                } catch (e){
+                    
+                }
+            }
+        }
+
+        // 
+
+    }
+    console.log(xmasMatches, samxMatches, xmasVertMatches, xmasDownRightMatches, xmasDownLeftMatches, samxVertMatches, samxDownRightMatches, samxDownLeftMatches);
+    console.log(horMatches);
+    console.log(totalMatches);
+    console.log("Part2 " + crossMas);
+    //1830 too low
+    //2461 too low
+    //2468 correct
+    //part 2 432 too low
+    //part 2 1864
+
+} catch (err) {
+    console.error(err);
+}
+//1830 too low
+/*else if (row[y] === 'S') {
                 //vertical case
                 if (y < rows.length - 3 && rows[y + 1][x] === "A" && rows[y + 2][x] === "M" &&  rows[y + 3][x] === "X") {
                     totalMatches++;
@@ -64,18 +132,4 @@ try {
                     totalMatches++;
                     samxDownLeftMatches++; //143
                 }
-            }
-        }
-        
-        // 
-        
-    }
-    console.log(xmasMatches, samxMatches, xmasVertMatches,xmasDownRightMatches,xmasDownLeftMatches,samxVertMatches,samxDownRightMatches,samxDownLeftMatches);
-    console.log(horMatches);
-    console.log(totalMatches);
-    // console.log(rows);
-
-} catch (err) {
-    console.error(err);
-}
-//1830 too low
+            }*/
